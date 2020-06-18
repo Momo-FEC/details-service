7fc3d1ed629f656534cfe1af79fd2e6aa21bdb88
Robert Bessler <peanutjackreigns@gmail.com>
2020-06-17 22:29:04 -0400

    Implements basic server sanity check

--- server/index.js
+++ server/index.js
@@ -1,1 +1,7 @@
- console.log('sanity check')
+const express = require('express');
+const app = express();
+const port = 3002;
+
+app.use(express.static('public'));
+
+app.listen(port, () => {console.log(`Details is listening on port ${port}`)});
