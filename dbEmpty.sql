93c44a417f28fa303e6e855a25193f9be9f66e99
Robert Bessler <peanutjackreigns@gmail.com>
2020-06-18 14:10:37 -0400

    Creates script for clearing out all tables and data from DB

---
+++ dbEmpty.sql
@@ -0,0 +1,9 @@
+use phoneDetails;
+
+DROP TABLE Phones_Capacities;
+DROP TABLE Phones_Carriers;
+DROP TABLE Phones_Colors;
+DROP TABLE Capacities;
+DROP TABLE Carriers;
+DROP TABLE Colors;
+DROP TABLE Phones;
