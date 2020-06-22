54651c4c1412bb0c3a57c82291a0925ff2ca24ee
Robert Bessler <peanutjackreigns@gmail.com>
2020-06-22 14:28:25 -0400

    Fixes consistency and linting issues

--- db.test.js
+++ db.test.js
@@ -12,11 +12,11 @@ require('mysql2/node_modules/iconv-lite').encodingExists('foo');
 // // resets the database (tears down and rebuilds)
 beforeEach(() => {
   return sequelize.sync({ force: true });
-})
+});
 
 afterAll(() => {
   return sequelize.close();
-})
+});
 
 it('should add data to all tables for first record added', () => {
   expect.assertions(4);
@@ -27,31 +27,31 @@ it('should add data to all tables for first record added', () => {
     carriers: ['carrierOne', 'carrierTwo'],
     colors: ['red', 'blue', 'green']
   })
-  .then(() => {
-    return Promise.all([Phone.findAll(),
-      Capacity.findAll(),
-      Carrier.findAll(),
-      Color.findAll()])
-  })
-  .then(([phones, capacities, carriers, colors]) => {
-    expect(phones.length).toBe(1);
-    expect(capacities.length).toBe(2);
-    expect(carriers.length).toBe(2);
-    expect(colors.length).toBe(3);
-  })
-  .catch(() => {
-    console.log('an error was caught');
-  });
+    .then(() => {
+      return Promise.all([Phone.findAll(),
+        Capacity.findAll(),
+        Carrier.findAll(),
+        Color.findAll()]);
+    })
+    .then(([phones, capacities, carriers, colors]) => {
+      expect(phones.length).toBe(1);
+      expect(capacities.length).toBe(2);
+      expect(carriers.length).toBe(2);
+      expect(colors.length).toBe(3);
+    })
+    .catch(() => {
+      console.log('an error was caught');
+    });
 });
 
-test('should add 100 more phone records after seed', () => {
+it('should add 100 more phone records after seed', () => {
   expect.assertions(4);
   return require('./database/seed.js')()
     .then(() => {
       return Promise.all([Phone.findAll(),
-            Capacity.findAll(),
-            Carrier.findAll(),
-            Color.findAll()])
+        Capacity.findAll(),
+        Carrier.findAll(),
+        Color.findAll()]);
     })
     .then(([phones, capacities, carriers, colors]) => {
       expect(phones.length).toBe(100);
@@ -59,8 +59,8 @@ test('should add 100 more phone records after seed', () => {
       expect(carriers.length).toBeLessThan(10);
       expect(colors.length).toBeLessThan(10);
     })
-    .catch(() => {})
-})
+    .catch(() => {});
+});
 
 it('should retrieve data for a given ID', () => {
   expect.assertions(3);
@@ -72,7 +72,7 @@ it('should retrieve data for a given ID', () => {
     colors: ['red', 'blue', 'green']
   })
     .then(() => {
-      return axios.get('http://localhost:3002/phones/1', { adapter })
+      return axios.get('http://localhost:3002/phones/1', { adapter });
     })
     .then((response) => {
       expect(typeof(response.data[0])).toBe('object');
@@ -81,5 +81,5 @@ it('should retrieve data for a given ID', () => {
     })
     .catch((err) => {
       expect(err).toBe('undefined');
-    })
-})
+    });
+});
