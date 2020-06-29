b9ad8849ffc6fa6c03512496fa20dde633858399
Robert Bessler <peanutjackreigns@gmail.com>
2020-06-29 15:31:57 -0400

    Allows cross-origin access

--- server/index.js
+++ server/index.js
@@ -3,9 +3,15 @@ const app = express();
 const port = 3002;
 const db = require('../database/index.js');
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.redirect('/1');
 });
+
 app.use('/:id', express.static('public'));
 
 app.get('/phones/:id', (req, res) => {
