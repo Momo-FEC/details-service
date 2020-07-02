5ef79f937b8ebd88ac6b496e25232d036d518260
Robert Bessler <peanutjackreigns@gmail.com>
2020-07-02 14:32:23 -0400

    Adds base button with no styling for ratings popup

--- client/Ratings.jsx
+++ client/Ratings.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
-var Filled = styled(({ className }) => {
+var Star = styled(({ className }) => {
   return (<span className={className}>★</span>);
 })`
-  color: #FECE30;
-`;
-
-var Empty = styled(({ className }) => {
-  return (<span className={className}>★</span>);
-})`
-  color: #CACACA;
+  color: ${props => props.fill ? '#FECE30' : '#CACACA'};
 `;
 
 var Average = styled(({ className }) => {
@@ -24,37 +18,60 @@ var Average = styled(({ className }) => {
 
 var Bar = styled.div`
   display: inline-block;
-  background-color: ${props => props.fill ? '#FECE30' : '#CACACA'};
-  width: ${props => props.width};
-  border-radius: ${props => props.fill ? '4px 0 0 4px' : '0 4px 4px 0'};
+  position: relative;
+  top: 2px;
   height: 12px;
+  width: ${props => props.width};
+  background-color: ${props => props.fill ? '#FECE30' : '#CACACA'};
+  border-radius: ${props => props.fill ? '4px 0 0 4px' : '0 4px 4px 0'};
+  margin-${props => props.fill ? 'left' : 'right'}: 10px;
 `;
 
 var Line = styled(({ className, index, count, total }) => {
   return (
     <div className={className}>
-      {index + 1}
-      <Filled />
+      {index}
+      <Star fill={true} />
       <Bar fill={true} width={(count / total * 60).toString() + '%'}/>
       <Bar fill={false} width={((total - count) / total * 60).toString() + '%'}/>
+      {count}
     </div>
   );
 })`
+  height: 25px;
+  font-size: 13px;
+`;
 
+var ReadReviews = styled(({ className, total }) => {
+  return (
+    <button className={className}>READ {total} REVIEWS</button>
+  );
+})`
+  color: red;
 `;
 
 var Popup = styled(({ className }) => {
+  var ratings = [2, 3, 4, 4, 2];
+  var total = ratings.reduce((x, y) => x + y);
   return (
     <div className={className}>
-      <Line index={1} count={4} total={10} />
+      {ratings.map((rating, index) => {
+        return (
+          <Line key={index} index={5 - index} count={rating} total={total} />
+        );
+      })}
+      <ReadReviews total={total} />
     </div>
   );
 })`
   position: absolute;
+  visibility: ${props => props.visible ? 'visible' : 'hidden'};
   top: 40px;
   width: 270px;
   padding: 5%;
   border: 1px solid black;
+  color: black;
+  text-align: center;
   background-color: white;
 `;
 
@@ -62,7 +79,7 @@ class Ratings extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      hover: true
+      hover: false
     };
   }
 
@@ -76,9 +93,10 @@ class Ratings extends React.Component {
       <div className={className}
         onMouseEnter={this.handleHover.bind(this)}
         onMouseLeave={this.handleHover.bind(this)}>
-        <Filled /><Filled /><Filled /><Empty /><Empty />
+        <Star fill={true} /><Star fill={true} /><Star fill={true} />
+        <Star fill={false} /><Star fill={false} />
         <Average />
-        {this.state.hover && <Popup />}
+        <Popup visible={this.state.hover} />
       </div>
     );
   }
