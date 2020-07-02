b9bb1754cf00ee4b582e5b780fab9171d92a1b62
Robert Bessler <peanutjackreigns@gmail.com>
2020-07-01 20:24:13 -0400

    Adds styling framework for rating hover popup

--- client/Ratings.jsx
+++ client/Ratings.jsx
@@ -22,16 +22,70 @@ var Average = styled(({ className }) => {
   font-size: 13px;
 `;
 
-var Ratings = ({ className }) => {
+var Bar = styled.div`
+  display: inline-block;
+  background-color: ${props => props.fill ? '#FECE30' : '#CACACA'};
+  width: ${props => props.width};
+  border-radius: ${props => props.fill ? '4px 0 0 4px' : '0 4px 4px 0'};
+  height: 12px;
+`;
+
+var Line = styled(({ className, index, count, total }) => {
   return (
     <div className={className}>
-      <Filled /><Filled /><Filled /><Empty /><Empty />
-      <Average />
+      {index + 1}
+      <Filled />
+      <Bar fill={true} width={(count / total * 60).toString() + '%'}/>
+      <Bar fill={false} width={((total - count) / total * 60).toString() + '%'}/>
     </div>
   );
-};
+})`
+
+`;
+
+var Popup = styled(({ className }) => {
+  return (
+    <div className={className}>
+      <Line index={1} count={4} total={10} />
+    </div>
+  );
+})`
+  position: absolute;
+  top: 40px;
+  width: 270px;
+  padding: 5%;
+  border: 1px solid black;
+  background-color: white;
+`;
+
+class Ratings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: true
+    };
+  }
+
+  handleHover() {
+    this.setState({hover: !this.state.hover});
+  }
+
+  render() {
+    var { className } = this.props;
+    return (
+      <div className={className}
+        onMouseEnter={this.handleHover.bind(this)}
+        onMouseLeave={this.handleHover.bind(this)}>
+        <Filled /><Filled /><Filled /><Empty /><Empty />
+        <Average />
+        {this.state.hover && <Popup />}
+      </div>
+    );
+  }
+}
 
 export default styled(Ratings)`
+  position: relative;
   display: inline-block;
   color: #0077C8;
   font-size: 21px;
